df91efe403de273d23e94b4c90f4de80b1c01bb8
dllarkin <dllarkin@uark.edu>
2018-04-09 17:22:31 -0500

    It works! (For now...)

--- parseGFF.py
+++ parseGFF.py
@@ -11,23 +11,40 @@
 #    print to screen
 # close the .gff file
 
-from BCBio import GFF
-from Bio import SeqIO
+# from BCBio import GFF
+# from Bio import SeqIO
 
-in_seq_file = "watermelon.fsa"
-in_seq_handle = open(in_seq_file)
-seq_dict = SeqIO.to_dict(SeqIO.parse(in_seq_handle, "fasta"))
-in_seq_handle.close()
+#in_seq_file = "watermelon.fsa"
+#in_seq_handle = open(in_seq_file)
+#seq_dict = SeqIO.to_dict(SeqIO.parse(in_seq_handle, "fasta"))
+#in_seq_handle.close()
 
-in_file = "watermelon.gff"
-in_handle = open(in_file)
-for record in GFF.parse(in_handle, base_dict=seq_dict):
-    print(record)
-in_handle.close()
+#in_file = "watermelon.gff"
+#in_handle = open(in_file)
+#for record in GFF.parse(in_handle, base_dict=seq_dict):
+#    print(record)
+#in_handle.close()
 
 # (species_name, type, begin, end) = line.split('\t')
 
 
+from Bio import SeqIO
+import csv
+import argparse
+genome = SeqIO.read("watermelon.fsa", "fasta")
+print("seqence is: ", len(genome.seq))
 
+gff = open("watermelon.gff", "r")
+for line in gff:
+	words = line.split("\t")
+	species = (words[0])
+	start = int(words[3])
+	end = int(words[4])
+	gene = (words[8])
+	print(">" + species + " | " + gene + genome.seq[start-1:end])
+gff.close()
 
-
+#for seq_record in SeqIO.parse("watermelon.fsa", "fasta"):
+#	print(seq_record.id)
+#	print(repr(seq_record.seq))
+#	print(len(seq_record))
